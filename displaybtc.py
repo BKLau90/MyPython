f2755d6798f1cb22661f7b60e6d5eed148b38658
BK Lau <bklau90@gmail.com>
2021-10-13 00:09:08 +0800

    changed api provider

--- displaybtc.py
+++ displaybtc.py
@@ -1,15 +1,32 @@
 #description: This program gets the price of crytocurrency in real time
 
-#import library to allow sent HTTP requests using Python
-import requests
+#import libraris 
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+#this line calls the coinmarketcap API to grab the current price of the BTC
 
-#this line calls the coindesk API to grab the current price of the cryto coin
-response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '7ec7fdc9-c101-42d2-8011-d91d632549d4',
+}
 
 #assign the results to variable data
 data = response.json()
 
-#display the data 
-print ("BTC Current Price is ")
-print(data["bpi"]["USD"]["rate"] )
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
 
