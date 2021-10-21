364d0908f68eb2901e6eab1f2caff0007f95d41a
BK Lau <bklau90@gmail.com>
2021-10-22 00:31:57 +0800

    added new api and table

--- displaycrypto.py
+++ displaycrypto.py
@@ -1,41 +1,34 @@
-#description: This program gets the price of crytocurrency in real time
+# description: This program gets the price of crytocurrency in real time
 
-#import libraris 
-from re import X
-from requests import Request, Session
-from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
-from requests.models import ContentDecodingError
+# import libraries
+import configparser
+from os import PRIO_PROCESS
+import requests
 from rich.console import Console
 from rich.table import Column, Table
+from configparser import ConfigParser
+import argparse
 import json
 ##############################################################################
 
-#Connection to Coinmarketcap API
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-headers = {
-   'Accepts': 'application/json',
-   'X-CMC_PRO_API_KEY': '7ec7fdc9-c101-42d2-8011-d91d632549d4',
-  }
-session = Session()
-session.headers.update(headers)
+api_request = requests.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+api_response = json.loads(api_request.content)
+my_portfolio = [
+    {
+        "sym": "btc"
+    }
+]
+for coin in api_response:
+    for mycoin in my_portfolio:
+        if mycoin["sym"] == coin["symbol"]:
+            crypt_coin = coin["name"]
+            rank = coin["market_cap_rank"]
+            price = coin["current_price"]
 
-response = session.get(url)
-data = json.loads(response.text)
-
-console = Console()
-
-for coin in data['data']:
-  symbol = (coin['symbol'])
-  rank = (coin['cmc_rank'])
-  price =(coin['quote']['USD']['price'])
-
-table = Table (title="My CryptoCurrency Portfolio")
+console = Console()           
+table = Table(title="My CryptoCurrency Portfolio")
 table.add_column("Name", justify="right", style="cyan", no_wrap=True)
 table.add_column("Rank", justify="right", style="cyan", no_wrap=True)
-table.add_column("Current Price:", justify="right", style="cyan", no_wrap=True)
-table.add_row(str(symbol),str(rank),(str(price)))
+table.add_column("Price", justify="right", style="cyan", no_wrap=True)
+table.add_row(str(crypt_coin), str(rank), (str(price)))
 console.print(table)
-
-
-
-
