74c64042fa083e67a6bce7b812bcb6149438a246
BK Lau <bklau90@gmail.com>
2021-10-22 01:44:40 +0800

    updated config

--- displaycrypto.py
+++ displaycrypto.py
@@ -6,29 +6,39 @@ from os import PRIO_PROCESS
 import requests
 from rich.console import Console
 from rich.table import Column, Table
+from rich.live import Live
 from configparser import ConfigParser
 import argparse
 import json
 ##############################################################################
 
-api_request = requests.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+api_request = requests.get(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
 api_response = json.loads(api_request.content)
+
 my_portfolio = [
     {
         "sym": "btc"
+    },
+    {
+        "sym": "eth"
+    },
+    {
+        "sym": "xrp"
     }
+
+
 ]
+console = Console()
+table = Table(title="My CryptoCurrency Portfolio")
+table.add_column("Name", justify="center", style="cyan", no_wrap=True)
+table.add_column("Rank", justify="center", style="cyan", no_wrap=True)
+table.add_column("Price", justify="center", style="cyan", no_wrap=True)
 for coin in api_response:
     for mycoin in my_portfolio:
         if mycoin["sym"] == coin["symbol"]:
-            crypt_coin = coin["name"]
+            name = coin["name"]
             rank = coin["market_cap_rank"]
-            price = coin["current_price"]
-
-console = Console()           
-table = Table(title="My CryptoCurrency Portfolio")
-table.add_column("Name", justify="right", style="cyan", no_wrap=True)
-table.add_column("Rank", justify="right", style="cyan", no_wrap=True)
-table.add_column("Price", justify="right", style="cyan", no_wrap=True)
-table.add_row(str(crypt_coin), str(rank), (str(price)))
+            price = "${:,.2f}".format(coin["current_price"])
+            table.add_row(str(name), str(rank), str(price))
 console.print(table)
